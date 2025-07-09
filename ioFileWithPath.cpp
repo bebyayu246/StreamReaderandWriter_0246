374bf4fcb6d6095a41175b1959208e930e725663
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:49:04 +0700

    Kode ini membaca dan menampilkan isi file yang sebelumnya ditulis oleh pengguna, dan akan menampilkan pesan error jika file tidak bisa dibuka.

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -31,3 +31,19 @@ int main() {
     infile.open(NamaFile, ios::in);
     cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
+     if(infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tuutp file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}
+
