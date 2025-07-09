1ecb57d2423fef06843021e2e571a758976523df
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:23:10 +0700

    mencoba menjalankan kode yang mungkin menyebabkan error.

--- ManualExeption.cpp
+++ ManualExeption.cpp
@@ -2,3 +2,9 @@
 using namespace std;
 
 int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
