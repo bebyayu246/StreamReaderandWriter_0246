1a471f4605fbd2543af9f8980f6626a48d21bad9
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:30:22 +0700

    menangkap error saat mengakses indeks array yang tidak ada, dan mencetak pesan error ke layar.

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -12,3 +12,8 @@ int main()
         array<int, 3> data = {10, 20, 30}; //pesan array integer 3 elemen
         cout <<data.at(5)<<endl; //memanggil array elemen ke 5
     }
+
+    catch (exception& e) { //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
