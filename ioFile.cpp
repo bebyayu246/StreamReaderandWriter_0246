1efe3d93cf6d294bcfc3a18fafc8724d715c0a54
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:40:54 +0700

    menutup file ,menunjukkan sebuah file dan menjalankan file tersebut

--- ioFile.cpp
+++ ioFile.cpp
@@ -20,3 +20,11 @@ int main() {
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+        outfile.close();
+        //Membuka file dalam mode membaca
+        ifstream infile;
+        //menunjuk ke sebuah file
+        infile.open("contohfile.txt");
+        cout << endl << ">= Pembuka  dan membaca file " << endl;
+        //jika file ada maka
