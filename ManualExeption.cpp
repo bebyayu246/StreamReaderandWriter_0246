c132e60e0080f70f4b6b0ec1bf2bb6e1dcc5124c
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:24:29 +0700

    Blok catch (...) digunakan untuk menangkap semua jenis exception, tidak peduli tipe datanya apa.

--- ManualExeption.cpp
+++ ManualExeption.cpp
@@ -13,3 +13,10 @@ int main()
         //blok ini akan dieksekusi
         cout << "Pengecualian akan dieksekusi" << endl;
     }
+
+    catch (...) {
+        /*jika selain integer maka blok ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
