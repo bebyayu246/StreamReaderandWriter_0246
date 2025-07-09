577031995f66cf3e9566a36778593267c3c678bd
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-07-09 17:28:58 +0700

    mendeklarasikan array dengan 3 element dan mencoba mengakses elemen ke-5

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -8,3 +8,7 @@ using namespace std;
 int main()
 {
     cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {10, 20, 30}; //pesan array integer 3 elemen
+        cout <<data.at(5)<<endl; //memanggil array elemen ke 5
+    }
